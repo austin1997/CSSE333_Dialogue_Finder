1dcf85e6982a978f91ee0dd1f7d5c24e890cf38e
YizhiFeng <sampsonfeng1996@gmail.com>
2017-02-21 15:11:07 -0500

    fix bugs in testEchoTable

--- testEchoTable.php
+++ testEchoTable.php
@@ -4,66 +4,244 @@
 //	Open	a	connection	to	the	database	
 //	(display	an	error	if	the	connection	fails)	
 header("Content-type: text/html; charset=utf-8");
-$serverName = "137.112.104.37"; //数据库服务器地址
-$uid = "zhaiz"; //数据库用户名
-$pwd = "555888austin"; //数据库密砿
+$serverName = "137.112.104.37";
+$uid = $_COOKIE['uid'];
+$pwd = $_COOKIE['pwd'];
 $connectionInfo = array("UID"=>$uid, "PWD"=>$pwd, "Database"=>"YFZZ", "CharacterSet"=>"UTF-8");
 $conn = sqlsrv_connect( $serverName, $connectionInfo);
 ?>
+
 <head>
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" type="text/javascript"></script>
-<title>Welcome!</title>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<title>Dialog Finder</title>
+<!-- mdl -->
+<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+<link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css">
+<script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
+
+<style>
+    .mdl-data-table {
+        width:100%;
+    }
+    .mdl-textfield {
+        margin: 0 auto;
+		text-align: center;
+    }
+</style>
 </head>
+
 <body>
-<form action="" method="post">
-				<label for="id">ID</label><br/>
-				<input type="text" name="id"/><br/>
-				<input type="submit" value="Search"/>
-</form>
-<table class="table table-hover">
-			<thead>
-			<tr>
-				<th>Posts table</th>
-			</tr>
-			</thead>
-			<tbody>
-			<tr>
-				<td>Name</td>
-				<td>ID</td>
-				<td>Body</td>
-			</tr>
-			
-			<?php
-	$id='0';	
-	if	($_SERVER['REQUEST_METHOD']	== 'POST')	{
-		$errors = '';
-		$id = $_POST['id'];
-		if	(empty($id))	$errors .= '<li>ID	is	required</li>';
-		if	(!empty($errors))	{	
-			echo '<ul>' . $errors . '</ul>';
-		}
-	}
-	if(!empty($id)){
-//		echo 'id is: '.$id.'<br>';
-		$posts = sqlsrv_query($conn, "SELECT Id, Episode_id, Content_CHN FROM Dialogue WHERE Id = $id");
-//		echo 'posts are: '.$posts.'<br>';
-		if (!($posts)) {
-			echo 'The anime you searched is not available';
-		}else{
-			while ($row = sqlsrv_fetch_array($posts))	{
-				echo '<tr><td>'.$row[0].'</td><td>'.$row[1].'</td><td>'.$row[2].'</td></tr>';
-			}
-		}
-	}else{
-		echo 'Please search for a anime.';
-	}
-		
-?>
-			
-			</tbody>
-	</table>
 
+<div class="mdl-layout mdl-js-layout mdl-layout--fixed-header
+            mdl-layout--fixed-tabs">
+    <header class="mdl-layout__header">
+        <div class="mdl-layout__header-row">
+            <!-- Title -->
+            <span class="mdl-layout-title">Dialog Finder</span>
+        </div>
+        <!-- Tabs -->
+        <div class="mdl-layout__tab-bar mdl-js-ripple-effect">
+            <a href="#fixed-tab-1" class="mdl-layout__tab is-active">Anime</a>
+            <a href="#fixed-tab-2" class="mdl-layout__tab">Episode</a>
+            <a href="#fixed-tab-3" class="mdl-layout__tab">Dialogue</a>
+            <a href="#fixed-tab-4" class="mdl-layout__tab">Search</a>
+        </div>
+    </header>
+    <main class="mdl-layout__content">
+        <section class="mdl-layout__tab-panel is-active" id="fixed-tab-1">
+            <div class="page-content">
+				<form action="/index - Copy.php" style="float:left;padding-top:20px;padding-left:20px;">
+					<button type="submit" class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect">Modify</button>
+					<?php
+						setcookie('changeTable','Anime');
+					?>
+				</form> 
 
+			<table class="mdl-data-table mdl-js-data-table mdl-shadow--2dp">
+				<thead>
+					<tr>
+						<th class="mdl-data-table__cell--non-numeric">Name</th>
+						<th>Date Released</th>
+						<th>Producer</th>
+						<th>Director</th>
+					</tr>
+				</thead>
+				<tbody>
+					<?php
+						$animeName='';	
+						if	($_SERVER['REQUEST_METHOD']	== 'POST')	{
+							if (isset($_POST['animeName'])) {
+								$animeName = $_POST['animeName'];
+							}
+						}
+						if(!empty($animeName)){
+							$posts = sqlsrv_query($conn, "EXEC search_Anime N'$animeName'");
+							if (!($posts) || $posts == 1) {
+								echo 'The anime you searched is not available';
+							}else{
+								while ($row = sqlsrv_fetch_array($posts))	{
+									echo '<tr><td class="mdl-data-table__cell--non-numeric">'.$row[0].'</td><td>'.date_format($row[1], 'd/m/y').'</td><td>'.$row[2].'</td><td>'.$row[3].'</td></tr>';
+								}
+							}
+						}else{
+							$posts = sqlsrv_query($conn, "EXEC search_Anime");
+							if (!($posts) || $posts == 1) {
+								echo 'Sorry! There is no anime existed.';
+							}else{
+								while ($row = sqlsrv_fetch_array($posts))	{
+									echo '<tr><td class="mdl-data-table__cell--non-numeric">'.$row[0].'</td><td>'.date_format($row[1], 'd/m/y').'</td><td>'.$row[2].'</td><td>'.$row[3].'</td></tr>';
+								}
+							}
+						}
+						?>
+				</tbody>
+			</table>
+				
+			</div>
+		</section>
+	<section class="mdl-layout__tab-panel" id="fixed-tab-2">
+		<div class="page-content">
+
+			<table class="mdl-data-table mdl-js-data-table mdl-shadow--2dp">
+				<thead>
+					<tr>
+						<th class="mdl-data-table__cell--non-numeric">UniqueID</th>
+						<th>Anime</th>
+						<th>Number</th>
+						<th>Summary</th>
+					</tr>
+				</thead>
+				<tbody>
+					<?php
+						$fromAnimeName='';	
+						if	($_SERVER['REQUEST_METHOD']	== 'POST')	{
+							$fromAnimeName = $_POST['fromAnimeName'];
+						}
+						if(!empty($fromAnimeName)){
+							$posts1 = sqlsrv_query($conn, "EXEC search_Episode N'$fromAnimeName'");
+					//		echo 'posts are: '.$posts1.'<br>';
+							if (!($posts1)) {
+								echo 'The anime you searched is not available';
+							}else{
+								while ($row = sqlsrv_fetch_array($posts1))	{
+									echo '<tr><td class="mdl-data-table__cell--non-numeric">'.$row[0].'</td><td>'.$row[1].'</td><td>'.$row[2].'</td><td>'.$row[3].'</td></tr>';
+								}
+							}
+						}else{
+							$posts1 = sqlsrv_query($conn, "EXEC search_Episode");
+					//		echo 'posts are: '.$posts1.'<br>';
+							if (!($posts1)) {
+								echo 'The anime you searched is not available';
+							}else{
+								while ($row = sqlsrv_fetch_array($posts1))	{
+									echo '<tr><td class="mdl-data-table__cell--non-numeric">'.$row[0].'</td><td>'.$row[1].'</td><td>'.$row[2].'</td><td>'.$row[3].'</td></tr>';
+								}
+							}
+						}
+						?>
+				</tbody>
+			</table>
+		</div>
+	</section>
+	<section class="mdl-layout__tab-panel" id="fixed-tab-3">
+		<div class="page-content">
+
+			<table class="mdl-data-table mdl-js-data-table mdl-shadow--2dp">
+				<thead>
+					<tr>
+						<th class="mdl-data-table__cell--non-numeric">Time</th>
+						<th>Content_JP</th>
+						<th>Content_CHN</th>
+					</tr>
+				</thead>
+				<tbody>
+					<?php
+						$dialogEpisode='';
+						$dialogKW='';	
+						if	($_SERVER['REQUEST_METHOD']	== 'POST')	{
+							$dialogEpisode = $_POST['dialogEpisode'];
+							$dialogKW=$_POST['dialogKW'];
+							//echo $dialogKW;
+						}
+						if(!empty($dialogEpisode)){
+							if(!empty($dialogKW)){
+								$posts2 = sqlsrv_query($conn, "SELECT Time, Content_JP, Content_CHN FROM Dialogue WHERE Episode_id = N'$dialogEpisode' AND Content_JP LIKE N'%$dialogKW%'");
+							//echo 'posts are: '.$posts.'<br>';
+							if (!($posts2)) {
+								echo 'Matched dialogue not found';
+							}else{
+								while ($row = sqlsrv_fetch_array($posts2))	{
+									echo '<tr><td class="mdl-data-table__cell--non-numeric">'.date_format($row[0], 'H:i:s').'</td><td>'.$row[1].'</td><td>'.$row[2].'</td></tr>';
+								}
+							}
+							}else{
+							$posts2 = sqlsrv_query($conn, "SELECT Time, Content_JP, Content_CHN FROM Dialogue WHERE Episode_id = N'$dialogEpisode'");
+							//echo 'posts are: '.$posts.'<br>';
+							if (!($posts2)) {
+								echo 'Matched dialogue not found';
+							}else{
+								while ($row = sqlsrv_fetch_array($posts2))	{
+									echo '<tr><td class="mdl-data-table__cell--non-numeric">'.date_format($row[0], 'H:i:s').'</td><td>'.$row[1].'</td><td>'.$row[2].'</td></tr>';
+								}
+							}
+							}
+						}else{
+							if(!empty($dialogKW)){
+								$posts2 = sqlsrv_query($conn, "SELECT Time, Content_JP, Content_CHN FROM Dialogue WHERE Content_JP LIKE N'%$dialogKW%'");
+								echo 'posts are: '.$posts.'<br>';
+								if (!($posts2)) {
+									echo 'Matched dialogue not found here';
+								}else{
+									while ($row = sqlsrv_fetch_array($posts2))	{
+										echo '<tr><td class="mdl-data-table__cell--non-numeric">'.date_format($row[0], 'H:i:s').'</td><td>'.$row[1].'</td><td>'.$row[2].'</td></tr>';
+									}
+								}
+							}else{
+								$posts2 = sqlsrv_query($conn, "EXEC search_Dialogue");
+								//echo 'posts are: '.$posts.'<br>';
+								if (!($posts2)) {
+									echo 'Sorry! There is no dialogue existed.';
+								}else{
+									while ($row = sqlsrv_fetch_array($posts2))	{
+										echo '<tr><td class="mdl-data-table__cell--non-numeric">'.date_format($row[0], 'H:i:s').'</td><td>'.$row[1].'</td><td>'.$row[2].'</td></tr>';
+									}
+								}
+							}
+						}
+						?>
+				</tbody>
+			</table>
+		</div>
+	</section>
+	<section class="mdl-layout__tab-panel" id="fixed-tab-4">
+		<div class="page-content">
+			<form action="" method="post">
+				<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+					<input class="mdl-textfield__input" type="text" id="anime_input" name="animeName">
+					<label class="mdl-textfield__label" for="anime_input">Anime Name</label>
+					</div>
+				<br>
+				<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+				<input class="mdl-textfield__input" type="text" id="episode_input" name="fromAnimeName">
+				<label class="mdl-textfield__label" for="episode_input">From which Anime</label>
+				</div>
+				<br>
+				<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+					<input class="mdl-textfield__input" type="text" id="dialog_input" name="dialogEpisode">
+					<label class="mdl-textfield__label" for="dialog_input">Episode Unique ID</label>
+				</div>
+				<br>
+				<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+					<input class="mdl-textfield__input" type="text" id="dialog_input2" name="dialogKW">
+					<label class="mdl-textfield__label" for="dialog_input2">Key Words</label>
+				</div>
+				<br>
+				<button type="submit" class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect">Search</button>
+			</form>
+		</div>
+	</section>
+</main>
+</div>
 </body>
 </html>
